6fa1d53c5d203cd1ad38393dc3d7232d9f89ca39
samaamro20 <samaamro20@gmail.com>
2019-01-16 13:37:20 +0200

    BackEnd service Page #3

--- src/backEnd/controllers/service.js
+++ src/backEnd/controllers/service.js
@@ -3,12 +3,11 @@ const { serviceDetails } = require('../../database/models');
 
 exports.get = (req, res) => {
 const { params: { id } } = req;
-serviceDetails.findAll({
-attributes: ['id', 'name'],
-raw: true
-})
+serviceDetails.findAll({ where : {serviceId : id },
+attributes: ['id', 'name', 'image_url'],
+raw: true })
 .then((result) => {
-res.render('halls', { result });
+res.render('service', { result });
 })
 .catch(() => {
 return res.status(500).json({ err: 'error in query' });
