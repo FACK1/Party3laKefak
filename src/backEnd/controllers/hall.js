3fee3b43a238f7fccd347f72ef044c35f9fa8f41
samaamro20 <samaamro20@gmail.com>
2019-01-17 15:43:19 +0200

    hall page BackEnd and simple style for it #11

--- src/backEnd/controllers/hall.js
+++ src/backEnd/controllers/hall.js
@@ -3,6 +3,14 @@ const { serviceDetails, media, user } = require('../../database/models');
 
 exports.get = (req, res) => {
 	const { params: { id } } = req;
+	// ////
+	// const details= result[0].service_detail.dataValues
+	// const images=    result.map((value)=>{
+	//     return value.image_url
+	//     })
+	//     const filteredResult= {images,details}
+	//         res.send(filteredResult);
+	// ///
 	media.findAll({
 		where: { serviceDetailId: id },
 		attributes: ['image_url'],
@@ -16,12 +24,13 @@ exports.get = (req, res) => {
 			 }],
 		  }],
 	}).then((result) => {
-		 // res.send(result);
-		 res.render('hall', { result });
+		const details = result[0].service_detail.dataValues;
+		const images = result.map(value => value.image_url);
+		const filteredResult = [{ images, details }];
+		// res.send(filteredResult);
+		 res.render('hall', { filteredResult });
 	})
 		.catch(() => res.status(500).json({ err: 'error in query' }));
-
-
 // serviceDetails.findAll({ where : {id : id },
 // attributes: ['name', 'image_url','location','description','price','map_loc'],
 // raw: true })
