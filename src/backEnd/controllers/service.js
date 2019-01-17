210bde83941debd9874a406a544fdc5ac60724b6
amanshawar <amanshawar_a@hotmail.com>
2019-01-17 13:27:48 +0200

    fix conflict

--- src/backEnd/controllers/service.js
+++ src/backEnd/controllers/service.js
@@ -1,15 +1,14 @@
-
 const { serviceDetails } = require('../../database/models');
 
 exports.get = (req, res) => {
-const { params: { id } } = req;
-serviceDetails.findAll({ where : {serviceId : id },
-attributes: ['id', 'name', 'image_url'],
-raw: true })
-.then((result) => {
-res.render('service', { result });
-})
-.catch(() => {
-return res.status(500).json({ err: 'error in query' });
-});
+  const { params: { id } } = req;
+  serviceDetails.findAll({
+      where: { serviceId: id },
+      attributes: ['id', 'name', 'image_url'],
+      raw: true,
+    })
+      .then((result) => {
+          res.render('service', { result });
+        })
+        .catch(() => res.status(500).json({ err: 'error in query' }));
 }
