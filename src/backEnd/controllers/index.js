04451dd6a92cdeabbd43ac7b5ea00163b8c99509
samaamro20 <samaamro20@gmail.com>
2019-01-20 16:05:56 +0200

    create signup and login style #13 #14

--- src/backEnd/controllers/index.js
+++ src/backEnd/controllers/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const services = require('./services');
 const service = require('./service');
 const hall = require('./hall');
+const signup = require('./signup');
+const login = require('./login');
+
 
 const router = express.Router();
 
@@ -9,6 +12,11 @@ router.get('/', services.get);
 router.get('/service/:id', service.get);
 router.get('/hall/:id', hall.get);
 router.get('/serverError');
+router.get('/signup', signup.get);
+router.post('/signup', signup.post);
+
+router.get('/login', login.get);
+router.post('/login', login.post);
 
 
 module.exports = router;
