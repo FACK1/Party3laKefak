8778069d4f3634f82963b4ed5b74e277ba5d9725
amanshawar <amanshawar_a@hotmail.com>
2019-01-21 09:07:30 +0200

    fix order of test

--- src/tests/test.js
+++ src/tests/test.js
@@ -1,8 +1,8 @@
 const tape = require('tape');
 
 tape('Testing tape', (t) => {
-const expected = 1;
-const accuual = 1;
-t.equal(accuual, expected, 'Should return empty string');
-t.end();
+  const expected = 1;
+  const accuual = 1;
+  t.equal(accuual, expected, 'Should return empty string');
+  t.end();
 });
