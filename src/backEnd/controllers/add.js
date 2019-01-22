06c838565ec87ac9814ffa56251dbbb3e64ce6f9
amanshawar <amanshawar_a@hotmail.com>
2019-01-22 15:05:05 +0200

    add into serives #21

--- src/backEnd/controllers/add.js
+++ src/backEnd/controllers/add.js
@@ -1,8 +1,39 @@
+const { serviceDetails, media } = require('../../database/models');
+
+const jwt = require('jsonwebtoken');
+const cookie = require('cookie');
+const { SECRET } = process.env;
 
 exports.get = (req, res) => {
   res.render('add');
 };
 
 exports.post = (req, res) => {
-  res.render('add');
+  const {
+    name, location, description, price, image_url
+  } = req.body;
+  if (req.headers.cookie) {
+    const token = cookie.parse(req.headers.cookie).logged_in;
+    jwt.verify(token, SECRET, (err, decoded) => {
+      if (err) { throw new Error(err); }
+      const userId = decoded.id;
+  serviceDetails.create({
+    name, location, description, price, image_url, userId, serviceId: 2,
+    raw: true,
+  })
+    .then((result) => {
+      media.create({
+        name, image_url, serviceDetailId: result.dataValues.id,
+      })
+        .then(() => {
+          res.render('add', { message: 'addded' });
+          return;
+        }).catch(() => {
+          res.render('serverError');
+        });
+    }).catch(() => {
+      res.render('serverError');
+    });
+      });
+  }
 };
