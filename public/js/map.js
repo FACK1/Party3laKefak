b3a7a2d9f009fd389284f66e2d99689a38573235
samaamro20 <samaamro20@gmail.com>
2019-01-23 10:54:40 +0200

    fix map

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,6 @@
 const att = document.getElementById('att');
 const loc = document.getElementById('loc');
-
+const L = 'http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js';
 if (att != null) {
   const cordenaites = att.innerHTML.split(',');
   const mymap = L.map('mapid').setView([cordenaites[0], cordenaites[1]], 17);
@@ -13,12 +13,12 @@ if (att != null) {
   mymap.on('click', onMapClick);
 
   function onMapClick(e) {
-    if (flag===0) {
-      loc.value=e.latlng.lat + ',' +e.latlng.lng ;
+    if (flag === 0) {
+      loc.value = `${e.latlng.lat},${e.latlng.lng}`;
       marker = L.marker(e.latlng).addTo(mymap);
-      flag=1;
-    }else if (flag ===1) {
-      flag=0;
+      flag = 1;
+    } else if (flag === 1) {
+      flag = 0;
       mymap.removeLayer(marker);
     }
   }
